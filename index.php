c5318b8d2f9caef2caee9a8aa74c6b5deca53c7f
trevone <trevorschuil@yahoo.com>
2012-09-29 22:14:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,25 +5,46 @@
         <title>Detect Colour Scheme</title>
         <style type="text/css">
             body{
-                background: #efefef;
+                background-color: #efefef;color:#00000;
             }
             #divRed{
-                width: 200px;
-                height: 200px;
-                background: #ff0000;
+                width: 200px;height: 200px;background-color: #ff0000;
             }
             #divGreen{
-                width: 200px;
-                height: 200px;
-                background: #00ff00;
+                width: 200px;height: 200px;background-color: #00ff00;
+            }
+            #div1{
+                width: 200px;height: 200px;background-color: #678e44;
+            }
+            #div2{
+                width: 200px;height: 200px;background-color: #834f00;
+            }
+            #div3{
+                width: 200px;height: 200px;background-color: #23500;
+            }
+            #colors{
+                position: absolute; top: 0px; right: 0px; padding: 10px; background: #cccccc;
+            }
+            .color{
+                width: 25px; height: 25px; border:1px solid #000000; margin:2px;
+            }
+            h3{
+                color:#336699;
             }
         </style>
+        <script type="text/javascript" src="jquery.js"></script>
+        <script type="text/javascript" src="DetectColour.js"></script>
     </head>
     <body>
         <div id="divRed"></div>
         <div id="divGreen"></div>
-        
-        <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
-        <script src="detectColour.js"></script>
+        <div id="div1"></div>
+        <div id="div2"><h3>Test</h3></div>
+        <div id="div3"><span id="tt">test</span></div>
+        <script type="text/javascript">
+        jQuery(document).ready(function() {
+            var theme = new DetectColor.Load();
+        });
+        </script>
     </body>
 </html>
