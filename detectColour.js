0def9b989401ae7787905234ad5f7e6d703bce8e
trevone <trevorschuil@yahoo.com>
2012-09-29 06:39:59 +0300

    Update detectColour.js

--- detectColour.js
+++ detectColour.js
@@ -1,94 +1,50 @@
 /* 
- * Detect Colour Scheme
+ * Detect Color Scheme
  * @Author Gerrit van Huyssteen
  * @Website www.gvh.co.za
  */
-
-(function detectColour(){
-       
-    var colours = new Array();
-    
-    // Create UI
-    jQuery('body').append(function(){
-        var uiHolder = '<div id="detectColourHolder" style="position: absolute; top: 0px; right: 0px; padding: 10px; background: #cccccc;"></div>';
-        return uiHolder;
-    })
-    
-    jQuery('*').each(function(){
-        /* for starters check your standards, I suggest crerate a class for all attrs */
-        var background = jQuery(this).css('background-color');
-        var colour = jQuery(this).css('color');
-        
-        colour = returnColour(colour);
-        background = returnColour(background);
-        
-        // you were probably testing for all kinds of array[0]'s? lol, start with the parent and work your way down.
-        // was it today that you shoke off progression enhancement like it was nothing? */
-        if( Object.prototype.toString.call( background ) === '[object Array]' ) {
-           groupColours(background[0]);
-        }
-        if( Object.prototype.toString.call( colour ) === '[object Array]' ) {
-           groupColours(colour[0]);
-        }
-        
-    });
-    
-    function returnColour(string){
-        return string.match(/ *\([^)]*\) */g);
-    }
-    
-    function groupColours(matchColour){
-        var matchCount = 0;
-        for(var i = 0; i <= colours.length; i++){
-
-            if(colours[i] == matchColour){
-                matchCount++;
+//this is how we roll
+DetectColor = (typeof DetectColor === 'undefined') ? {} : DetectColor;
+DetectColor.Load = function(  ) {
+        colors : new Array(),
+        DetectColor.Load.prototype = {
+            colors : new Array(),
+            hexDigits : ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"],
+            returnColor: function( string ) {
+       		string.match( / *\([^)]*\) */g );
+		},
+            groupColors:   function ( matchColor ){
+                    var matchCount = 0;
+                    for( i in this.colors ) if( this.colors[i] == matchColor ) matchCount++;
+                    if( matchCount == 0 ) this.colors[this.colors.length] = matchColor;
+                },
+            hasAlpha:   function ( attr ){
+                return ( attr.split(',')[3] == undefined) ? 'rgb' : 'rgba';
+            },
+            toHex: function ( x ) {
+                return isNaN( x ) ? "00" : this.hexDigits[(x - x % 16) / 16] + this.hexDigits[x % 16];
+            },
+            colorToHex: function ( rgbToColor ) {
+                if( this.hasAlpha( rgbToColor ) ) return rgbToColor;
+                if( Object.prototype.toString.call( rgbToColor.match(/(.*?)\((\d+), (\d+), (\d+)\)/) ) === '[object Array]' ) 
+                    return '#'+this.toHex( rgbToColor[2] )+this.toHex( rgbToColor[3] )+this.toHex( rgbToColor[4] );
+            },
+            draw: function(){
+                jQuery('*').each( function ( ) { 
+                    var background = jQuery( this ).css('background-color');
+                    var color = jQuery( this ).css('color');
+                    color = DetectColor.Load.prototype.returnColor( color );
+                    ( Object.prototype.toString.call( background ) === '[object Array]' ) ? 
+                        DetectColor.Load.prototype.groupColors( DetectColor.Load.prototype.returnColor( background )[0] ) : DetectColor.Load.prototype.groupColors( background );
+                    ( Object.prototype.toString.call( color ) === '[object Array]' ) ? 
+                        DetectColor.Load.prototype.groupColors( color[0] ) : DetectColor.Load.prototype.groupColors( background );                    
+                });
+                jQuery('body').append( '<div id="detectColorHolder" style="position: absolute; top: 0px; right: 0px; padding: 10px; background: #cccccc;"></div>');
+                    for( var i in DetectColor.Load.prototype.colors )
+                        $( '#detectColorHolder' ).append( function() {
+                            var hex = ( DetectColor.Load.prototype.colors[i].match('#') != 'null' ) ? DetectColor.Load.prototype.colors[i] : DetectColor.Load.prototype.colorToHex(DetectColor.Load.prototype.hasAlpha( DetectColor.Load.prototype.colors[i] )+''+DetectColor.Load.prototype.colors[i]) ;              
+                            return '<div style="width: 25px; height: 25px; border:1px solid #000000; margin:2px; background: '+hex+';" title="'+hex+'"></div>'; 
+                        });
             }
-        }
-        if(matchCount == 0){
-            var colCount = colours.length;
-            
-            colours[colCount] = matchColour;
-        }
-    }
-    
-    function hasAlpha(c){
-        c = c.split(',');
-        if(c[3] == undefined){
-            return 'rgb';
-        }else{
-            return 'rgba';
-        }
-    }
-    
-    //http://wowmotty.blogspot.com/2009/06/convert-jquery-rgb-output-to-hex-color.html
-    function toHex(x) {
-        var hexDigits = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
-        return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
-    }
-    
-    function colourToHex(rgbcol) {
-        var regex;
-        switch (hasAlpha(rgbcol)){
-            case 'rgba': return rgbcol;
-                break;
-            case 'rgb': regex = /(.*?)\((\d+), (\d+), (\d+)\)/;
-                break;
-        }
-        rgbcol = rgbcol.match(regex);
-        
-        var hex = '#'+toHex(rgbcol[2])+toHex(rgbcol[3])+toHex(rgbcol[4]);
-        return hex;
-    }
-    
-    
-    
-    (function addToHolder(){
-        for(var i = 0; i < colours.length; i++){
-            $('#detectColourHolder').append(function(){
-                var hex = colourToHex(hasAlpha(colours[i])+''+colours[i]);
-               return '<div style="width: 25px; height: 25px; border:1px solid #000000; margin:2px; background: '+hex+';" title="'+hex+'"></div>'; 
-            });
-        }
-    })();
-})();
+        },DetectColor.Load.prototype.draw() 
+};
