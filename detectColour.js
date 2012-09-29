42d50bcc3b17af39a223f36a6c8d7dea616a3513
trevone <trevorschuil@yahoo.com>
2012-09-29 22:12:13 +0300

    Update detectColour.js

--- detectColour.js
+++ detectColour.js
@@ -2,60 +2,126 @@
  * Detect Color Scheme
  * @Author Gerrit van Huyssteen
  * @Website www.gvh.co.za
- */
+ * @Contributer TrevorSchuil
+ * https://github.com/trevone/detect-colour-scheme
+ * 
+ * Abstract
+ * This is a concrete instance of the Scheme
+ * 
+ * Usage
+ * var scheme = DetectColor.Load();
+ * 
+ * Propterties
+ * colors - Array
+ * hasAlpha - Function
+ * toHex - Function
+ * rgbToHex - Function String
+ * removeDuplicates - Function Array
+ * draw - Function Array
+*/
+
 DetectColor = ( typeof DetectColor === 'undefined' ) ? {} : DetectColor;
-(function(){
+
+( function (){
+    
+    /* 
+    * Strip a string down to its color
+    * DetectColor.Load.returnColor( someColor );
+    */
     DetectColor.Load = function() {
-        this.colors = this.draw();
+        this.colors = this.draw(); 
     }
+    
+    /* 
+    * Extend DetectColor.Load with some default properties 
+    */
     DetectColor.Load.prototype = {
+        
         colors : [],
         attrs : ["border-color","background-color","color","font-color","outline-color"],
         hexDigits : ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"],
+        
+        /* 
+         * Strip a string down to its color
+         * DetectColor.Load.returnColor( someColor );
+         */
         returnColor: function( string ) {
             string.match( / *\([^)]*\) */g );
         },
-        groupColors:   function ( matchColor ){
+        
+        /* 
+         * this one needs an explaination
+         */
+        groupColors: function ( matchColor ){
             var matchCount = 0;
             for( var i in this.colors ) if( this.colors[i] == matchColor ) matchCount++;
-            if( matchCount == 0 && !!matchColor )this.colors.push(matchColor);
+            if( matchCount == 0 && !!matchColor ) this.colors.push( matchColor );
         },
-        hasAlpha:   function ( attr ){
+        
+        /* 
+         * Checks a color string for the alpha attribute
+         * DetectColor.Load.hasAlpha( someColor );
+         */
+        hasAlpha: function ( attr ){
             return ( attr.split(',')[3] == undefined) ? 'rgb' : 'rgba';
         },
-        toHex: function ( x ) {
-            return isNaN( x ) ? "00" : this.hexDigits[(x - x % 16) / 16] + this.hexDigits[x % 16];
+        
+        /* 
+         * Converts single value to Hex
+         * DetectColor.Load.toHex( 120 );
+         */
+        toHex: function ( value ) {
+            return isNaN( value ) ? "00" : this.hexDigits[ ( value - value % 16 ) / 16 ] + this.hexDigits[ value % 16];
         },
-        colorToHex: function ( rgbToColor ) {
-            if( this.hasAlpha( rgbToColor ) ) return rgbToColor;
-            if( Object.prototype.toString.call( rgbToColor.match( /(.*?)\((\d+), (\d+), (\d+)\)/ ) ) === '[object Array]' ) 
-                return '#'+this.toHex( rgbToColor[2] ) + this.toHex( rgbToColor[3] ) + this.toHex( rgbToColor[4] );
+        
+        /* 
+         * Converts rgb to Hex
+         * DetectColor.Load.rgbToHex('rgb(120, 120, 0));
+         */
+        rgbToHex: function ( rgb ) {
+            if( this.hasAlpha( rgb ) ) return rgb;
+            /* IE8 and below fail safe */
+            if( Object.prototype.toString.call( rgb.match( /(.*?)\((\d+), (\d+), (\d+)\)/ ) ) === '[object Array]' ) 
+                return '#'+this.toHex( rgb[2] ) + this.toHex( rgb[3] ) + this.toHex( rgb[4] );
         },
+        
+        /* 
+         * Used to remove duplicated colors
+         * DetectColor.Load.removeDuplicates(['#eee',#eee]);
+         */
         removeDuplicates: function ( array ) {
-            var i,len=array.length,out=[],obj={};
-            for ( var i = 0; i < len; i++ ) obj[ array[i] ] = 0;
-            for ( var i in obj ) out.push(i);
-            return out;
+            var i, uniques = [], object = {};
+            for ( var i = 0; i < array.length; i++ ) object[array[i]] = 0;
+            for ( var i in object ) uniques.push(i);
+            return uniques;
         },
+        
+        /* 
+         * Called from the constructor and returns all the stuff it created 
+         * DetectColor.Load.draw();
+         */
         draw: function(){
-            jQuery('*').each( function ( ) { 
+            jQuery('*').each( function () { 
                 for( var a in DetectColor.Load.prototype.attrs ){
                     var background = jQuery( this ).css(DetectColor.Load.prototype.attrs[a]);
+                    /* IE8 and below fail safe */
                     ( Object.prototype.toString.call( background ) === '[object Array]' ) ? 
                         DetectColor.Load.prototype.groupColors( DetectColor.Load.prototype.returnColor( background )[0] ) : DetectColor.Load.prototype.groupColors( background );
                 }
             });
-            jQuery('<div id="detectColorHolder" />').appendTo('body');
+            jQuery('<div id="colors" />').appendTo('body');
             DetectColor.Load.prototype.colors = DetectColor.Load.prototype.removeDuplicates ( DetectColor.Load.prototype.colors );
             for( var i in DetectColor.Load.prototype.removeDuplicates ( DetectColor.Load.prototype.colors ) )
-                jQuery( '#detectColorHolder' ).append( function() {
+                jQuery( '#colors' ).append( function() {
                     if( !!DetectColor.Load.prototype.colors[i] ){
                         var hex = ( DetectColor.Load.prototype.colors[i].match('#') != 'null' ) ? 
-                        DetectColor.Load.prototype.colors[i] : DetectColor.Load.prototype.colorToHex( DetectColor.Load.prototype.hasAlpha( DetectColor.Load.prototype.colors[i] ) + '' + DetectColor.Load.prototype.colors[i] ) ;              
-                        return '<div class="color" style="background-color:'+hex+';" />'; 
+                        DetectColor.Load.prototype.colors[i] : DetectColor.Load.prototype.rgbToHex( DetectColor.Load.prototype.hasAlpha( DetectColor.Load.prototype.colors[i] ) + '' + DetectColor.Load.prototype.colors[i] ) ;              
+                        return '<div class="color" style="background-color:' + hex + ';" />'; 
                     }
                 });
            return DetectColor.Load.prototype.colors;
         }
+        
     }
+    
 })()
