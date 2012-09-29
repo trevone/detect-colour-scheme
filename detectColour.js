8e86b71cf9a2d3890dedda4ce6ddbebaa135ad3b
trevone <trevorschuil@yahoo.com>
2012-09-29 19:25:41 +0300

    Update detectColour.js
    The beginnings of a simple inheritance model.

--- detectColour.js
+++ detectColour.js
@@ -3,48 +3,61 @@
  * @Author Gerrit van Huyssteen
  * @Website www.gvh.co.za
  */
-//this is how we roll
-DetectColor = (typeof DetectColor === 'undefined') ? {} : DetectColor;
-DetectColor.Load = function(  ) {
-        colors : new Array(),
-        DetectColor.Load.prototype = {
-            colors : new Array(),
-            hexDigits : ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"],
-            returnColor: function( string ) {
-       		string.match( / *\([^)]*\) */g );
-		},
-            groupColors:   function ( matchColor ){
-                    var matchCount = 0;
-                    for( i in this.colors ) if( this.colors[i] == matchColor ) matchCount++;
-                    if( matchCount == 0 ) this.colors[this.colors.length] = matchColor;
-                },
-            hasAlpha:   function ( attr ){
-                return ( attr.split(',')[3] == undefined) ? 'rgb' : 'rgba';
-            },
-            toHex: function ( x ) {
-                return isNaN( x ) ? "00" : this.hexDigits[(x - x % 16) / 16] + this.hexDigits[x % 16];
-            },
-            colorToHex: function ( rgbToColor ) {
-                if( this.hasAlpha( rgbToColor ) ) return rgbToColor;
-                if( Object.prototype.toString.call( rgbToColor.match(/(.*?)\((\d+), (\d+), (\d+)\)/) ) === '[object Array]' ) 
-                    return '#'+this.toHex( rgbToColor[2] )+this.toHex( rgbToColor[3] )+this.toHex( rgbToColor[4] );
-            },
-            draw: function(){
-                jQuery('*').each( function ( ) { 
-                    var background = jQuery( this ).css('background-color');
-                    var color = jQuery( this ).css('color');
-                    color = DetectColor.Load.prototype.returnColor( color );
+DetectColor = ( typeof DetectColor === 'undefined' ) ? {} : DetectColor;
+(function(){
+    DetectColor.Load = function() {
+        this.colors = this.draw();
+    }
+    DetectColor.Load.prototype = {
+        colors : [],
+        attrs : ["border-color","background-color","color","font-color","outline-color"],
+        hexDigits : ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"],
+        returnColor: function( string ) {
+            string.match( / *\([^)]*\) */g );
+        },
+        groupColors:   function ( matchColor ){
+            var matchCount = 0;
+            for( var i in this.colors ) if( this.colors[i] == matchColor ) matchCount++;
+            if( matchCount == 0 && !!matchColor ){
+                this.colors.push(matchColor);
+            }
+        },
+        hasAlpha:   function ( attr ){
+            return ( attr.split(',')[3] == undefined) ? 'rgb' : 'rgba';
+        },
+        toHex: function ( x ) {
+            return isNaN( x ) ? "00" : this.hexDigits[(x - x % 16) / 16] + this.hexDigits[x % 16];
+        },
+        colorToHex: function ( rgbToColor ) {
+            if( this.hasAlpha( rgbToColor ) ) return rgbToColor;
+            if( Object.prototype.toString.call( rgbToColor.match( /(.*?)\((\d+), (\d+), (\d+)\)/ ) ) === '[object Array]' ) 
+                return '#'+this.toHex( rgbToColor[2] ) + this.toHex( rgbToColor[3] ) + this.toHex( rgbToColor[4] );
+        },
+        removeDuplicates: function ( array ) {
+            var i,len=array.length,out=[],obj={};
+            for ( var i = 0; i < len; i++ ) obj[ array[i] ] = 0;
+            for ( var i in obj ) out.push(i);
+            return out;
+        },
+        draw: function(){
+            jQuery('*').each( function ( ) { 
+                for( var a in DetectColor.Load.prototype.attrs ){
+                    var background = jQuery( this ).css(DetectColor.Load.prototype.attrs[a]);
                     ( Object.prototype.toString.call( background ) === '[object Array]' ) ? 
                         DetectColor.Load.prototype.groupColors( DetectColor.Load.prototype.returnColor( background )[0] ) : DetectColor.Load.prototype.groupColors( background );
-                    ( Object.prototype.toString.call( color ) === '[object Array]' ) ? 
-                        DetectColor.Load.prototype.groupColors( color[0] ) : DetectColor.Load.prototype.groupColors( background );                    
+                }
+            });
+            jQuery('<div id="detectColorHolder" />').appendTo('body');
+            DetectColor.Load.prototype.colors = DetectColor.Load.prototype.removeDuplicates ( DetectColor.Load.prototype.colors );
+            for( var i in DetectColor.Load.prototype.removeDuplicates ( DetectColor.Load.prototype.colors ) )
+                jQuery( '#detectColorHolder' ).append( function() {
+                    if( !!DetectColor.Load.prototype.colors[i] ){
+                        var hex = ( DetectColor.Load.prototype.colors[i].match('#') != 'null' ) ? 
+                        DetectColor.Load.prototype.colors[i] : DetectColor.Load.prototype.colorToHex( DetectColor.Load.prototype.hasAlpha( DetectColor.Load.prototype.colors[i] ) + '' + DetectColor.Load.prototype.colors[i] ) ;              
+                        return '<div class="color" style="background-color:'+hex+';" />'; 
+                    }
                 });
-                jQuery('body').append( '<div id="detectColorHolder" style="position: absolute; top: 0px; right: 0px; padding: 10px; background: #cccccc;"></div>');
-                    for( var i in DetectColor.Load.prototype.colors )
-                        $( '#detectColorHolder' ).append( function() {
-                            var hex = ( DetectColor.Load.prototype.colors[i].match('#') != 'null' ) ? DetectColor.Load.prototype.colors[i] : DetectColor.Load.prototype.colorToHex(DetectColor.Load.prototype.hasAlpha( DetectColor.Load.prototype.colors[i] )+''+DetectColor.Load.prototype.colors[i]) ;              
-                            return '<div style="width: 25px; height: 25px; border:1px solid #000000; margin:2px; background: '+hex+';" title="'+hex+'"></div>'; 
-                        });
-            }
-        },DetectColor.Load.prototype.draw() 
-};
+           return DetectColor.Load.prototype.colors;
+        }
+    }
+})()
