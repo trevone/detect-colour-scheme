75e01a48e968cf0c1d366a687496d3bde52863a4
trevone <trevorschuil@yahoo.com>
2012-09-29 06:40:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
                 width: 200px;
                 height: 200px;
                 background-color: #ff0000;
-                
             }
             #divGreen{
                 width: 200px;
@@ -20,12 +19,18 @@
                 background-color: #00ff00;
             }
         </style>
+        <script type="text/javascript" src="jquery.js"></script>
+        <script type="text/javascript" src="detectColour.js"></script>
     </head>
     <body>
         <div id="divRed"></div>
         <div id="divGreen"></div>
-        
-        <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
-        <script src="detectColour.js"></script>
+        <script type="text/javascript">
+        jQuery(document).ready(function($) {
+            var detectColor = new DetectColor.Load();
+            console.log("Heres your object:");
+            console.log(detectColor);
+        });
+        </script>
     </body>
 </html>
